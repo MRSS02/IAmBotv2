65e8916d1b24e2df2920a5749286bd94bf577ee4
MRSS <mateusyasd@hotmail.com>
2021-05-31 00:41:08 -0300

    fixed package.json script bug

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']
 const token = process.env.TOKEN
 
 client.on('messageReactionAdd', async (reaction, user) => {
-  if (reaction.partial) { //this whole section just checks if the reaction is partial
+  if (reaction.partial) {
         try {
-            await reaction.fetch(); //fetches reaction because not every reaction is stored in the cache
+            await reaction.fetch();
         } catch (error) {
             console.error('Fetching message failed: ', error);
             return;
         }
     }
     if (!user.bot) {
-        if (reaction.emoji.id == 848749688424628224) { //if the user reacted with the right emoji
+        if (reaction.emoji.id == 848749688424628224) {
 
-            const role = reaction.message.guild.roles.cache.find(r => r.id == 847271805227106305); //finds role you want to assign (you could also user .name instead of .id)
+            const role = reaction.message.guild.roles.cache.find(r => r.id == 847271805227106305);
 
-            const { guild } = reaction.message //store the guild of the reaction in variable
+            const { guild } = reaction.message
 
-            const member = guild.members.cache.find(member => member.id === user.id); //find the member who reacted (because user and member are seperate things)
+            const member = guild.members.cache.find(member => member.id === user.id);
 
             if (reaction.message.id == 848697560943296542) {
-            member.roles.add(role); console.log("deu bom")//assign selected role to member
+            member.roles.add(role); console.log("deu bom")
             } else console.log("deu ruim")
 
         }
