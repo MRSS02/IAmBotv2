20bd0230195a6d7c6c7fe8d3f530f9ef560edb85
MRSS <mateusyasd@hotmail.com>
2021-05-31 11:45:37 -0300

    added noxp role

--- index.js
+++ index.js
@@ -3,29 +3,58 @@ const Discord = require("discord.js")
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 const token = process.env.TOKEN
 
+client.on("message", (message) => {
+  content = message.content
+  channelId = message.channel.id
+  if(content.includes("no xp role") && channelId == 848929953641791555)
+   message.react("<:noXP:848925909858910259>")
+})
+
 client.on('messageReactionAdd', async (reaction, user) => {
-  if (reaction.partial) {
-        try {
-            await reaction.fetch();
-        } catch (error) {
-            console.error('Fetching message failed: ', error);
-            return;
-        }
+    if (reaction.partial) {
+      try {
+          await reaction.fetch();
+      } catch (error) {
+          console.error('Fetching message failed: ', error);
+          return;
+      }
     }
     if (!user.bot) {
-        if (reaction.emoji.id == 848749688424628224) {
+      const clubbers = reaction.message.guild.roles.cache.find(r => r.id == 847271805227106305);
+      const noxp = reaction.message.guild.roles.cache.find(r => r.id == 848927759446442045);
 
-            const role = reaction.message.guild.roles.cache.find(r => r.id == 847271805227106305);
+      const { guild } = reaction.message
 
-            const { guild } = reaction.message
+      const member = guild.members.cache.find(member => member.id === user.id);
 
-            const member = guild.members.cache.find(member => member.id === user.id);
+      if (reaction.message.id == 848697560943296542
+      && reaction.emoji.id == 848749688424628224)
+      member.roles.add(clubbers); //add role clubers if ids match
+      else if (reaction.message.id == 848934354100813845
+      && reaction.emoji.id == 848925909858910259)
+      member.roles.add(noxp) //add role noexp if ids match
+    }
+});
 
-            if (reaction.message.id == 848697560943296542) {
-            member.roles.add(role); console.log("deu bom")
-            } else console.log("deu ruim")
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (reaction.partial) {
+      try {
+          await reaction.fetch();
+      } catch (error) {
+          console.error('Fetching message failed: ', error);
+          return;
+      }
+    }
+    if (!user.bot) {
+      const noxp = reaction.message.guild.roles.cache.find(r => r.id == 848927759446442045);
 
-        }
+      const { guild } = reaction.message
+
+      const member = guild.members.cache.find(member => member.id === user.id);
+
+      if (reaction.message.id == 848934354100813845
+      && reaction.emoji.id == 848925909858910259)
+      member.roles.remove(noxp) //remove role noexp if ids match
     }
 });
 
